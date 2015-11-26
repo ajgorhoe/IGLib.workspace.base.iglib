ecf95e29fec3f7c051daf3c06c54bbbdb9a05b6c
igor <igor@742cbac0-df1b-9c47-ab60-29ee275e43a9>
2015-11-26 09:56:28 +0000

    git-svn-id: https://localhost:8443/svn/ig_base/iglib/trunk@281 742cbac0-df1b-9c47-ab60-29ee275e43a9

---
+++ copylicences.bat
@@ -0,0 +1,23 @@
+
+echo off
+echo.
+echo.
+
+echo This will copy licene and other important doxuments to their intended places.
+
+set dirorig=.
+set diriglib=.\igbase
+set dirigforms=.\igforms
+
+copy /y %dirorig%\ReadMe_IGLib.html %diriglib%
+copy /y %dirorig%\License_IGLib_Partner.html %diriglib%
+copy /y %dirorig%\License_IGLib_Redistributable.html %diriglib%
+
+
+copy /y %dirorig%\ReadMe_IGLib.html %dirigforms%
+copy /y %dirorig%\License_IGLib_Partner.html %dirigforms%
+copy /y %dirorig%\License_IGLib_Redistributable.html %dirigforms%
+
+
+
+
