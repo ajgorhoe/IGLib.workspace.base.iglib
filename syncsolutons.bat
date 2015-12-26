03637fcf7084c00716c06f13199fb099d0e78ff2
igor <igor@742cbac0-df1b-9c47-ab60-29ee275e43a9>
2015-12-26 13:43:43 +0000

    git-svn-id: https://localhost:8443/svn/ig_base/iglib/trunk@357 742cbac0-df1b-9c47-ab60-29ee275e43a9

---
+++ syncsolutons.bat
@@ -0,0 +1,12 @@
+
+echo.
+echo Synchronizing solution files FROM their original locations in ..\solutions...
+echo.
+
+copy /y ..\igsolutions\IGLib.sln .\
+echo.
+
+echo. 
+echo.
+echo ... synchronization of solutions done.
+echo.
