12cda1b46f9e5e958ab0553434b957057f9b8ed5
Igor Grešovnik <gresovnik@gmail.com>
2020-12-17 11:12:43 +0100

    NPlot removed from igplot2d. Code builds fine.

--- extensions/igplot2d/plot2d/develop/nplot/GraphNPlot.cs
+++ extensions/igplot2d/plot2d/develop/nplot/GraphNPlot.cs
@@ -1,51 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.Threading;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Windows.Forms;
+//using System.Threading;
 
 
-using IG.Lib;
+//using IG.Lib;
 
-namespace IG.Plot2d
-{
+//namespace IG.Plot2d
+//{
 
-    /// <summary>NPlot graphs plotting object.</summary>
-    /// $A Igor Jun09;
-    public class GraphNPlot : GraphBase
-    {
+//    /// <summary>NPlot graphs plotting object.</summary>
+//    /// $A Igor Jun09;
+//    public class GraphNPlot : GraphBase
+//    {
 
-        /// <summary>Creates a new graph plotter based on a NPlot control.</summary>
-        /// <param name="control">Control used for plotting windows graphs.</param>
-        public GraphNPlot(PlotControlNPlot control)
-            : base("NPlot Graph", "NPlot Graph control.")
-        {
-        }
-
-
-        /// <summary>Creates a new graph plotter based on a NPlot control.</summary>
-        /// <param name="control">Control on which graphs are actually plotted.</param>
-        /// <param name="title">Title ofthe current plotter.</param>
-        /// <param name="description">Description of the current plotter.</param>
-        public GraphNPlot(PlotControlNPlot control, string title, string description): base(title, description)
-        {
-            this.Control = control;
-        }
+//        /// <summary>Creates a new graph plotter based on a NPlot control.</summary>
+//        /// <param name="control">Control used for plotting windows graphs.</param>
+//        public GraphNPlot(PlotControlNPlot control)
+//            : base("NPlot Graph", "NPlot Graph control.")
+//        {
+//        }
 
 
-        PlotControlNPlot _control;
-
-        /// <summary>2D plot control where graphic items are plotted.</summary>
-        public PlotControlNPlot Control
-        {
-            get { return _control; }
-            protected set { _control = value; }
-        }
-
-
+//        /// <summary>Creates a new graph plotter based on a NPlot control.</summary>
+//        /// <param name="control">Control on which graphs are actually plotted.</param>
+//        /// <param name="title">Title ofthe current plotter.</param>
+//        /// <param name="description">Description of the current plotter.</param>
+//        public GraphNPlot(PlotControlNPlot control, string title, string description): base(title, description)
+//        {
+//            this.Control = control;
+//        }
 
 
-    }
+//        PlotControlNPlot _control;
 
-}
+//        /// <summary>2D plot control where graphic items are plotted.</summary>
+//        public PlotControlNPlot Control
+//        {
+//            get { return _control; }
+//            protected set { _control = value; }
+//        }
+
+
+
+
+//    }
+
+//}
--- extensions/igplot2d/plot2d/develop/nplot/GraphWindowNPlot.cs
+++ extensions/igplot2d/plot2d/develop/nplot/GraphWindowNPlot.cs
@@ -1,26 +1,26 @@
-﻿// Copyright (c) Igor Grešovnik (2009), IGLib license; http://www2.arnes.si/~ljc3m2/igor/ioptlib/
+﻿//// Copyright (c) Igor Grešovnik (2009), IGLib license; http://www2.arnes.si/~ljc3m2/igor/ioptlib/
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Windows.Forms;
 
 
-namespace IG.Plot2d
-{
+//namespace IG.Plot2d
+//{
 
-    /// <summary>Stand-alone graph window using NPlot as plotting engine.
-    /// For layout, GraphWindowTemplate class is inherited.</summary>
-    /// $A Igor Sept09;
-    public partial class GraphWindowNPlot : GraphWindowTemplate
-    {
-        public GraphWindowNPlot()
-        {
-            InitializeComponent();
-        }
-    }
-}
+//    /// <summary>Stand-alone graph window using NPlot as plotting engine.
+//    /// For layout, GraphWindowTemplate class is inherited.</summary>
+//    /// $A Igor Sept09;
+//    public partial class GraphWindowNPlot : GraphWindowTemplate
+//    {
+//        public GraphWindowNPlot()
+//        {
+//            InitializeComponent();
+//        }
+//    }
+//}
