ef27c1a936a0664b4280f8fc0b9a3a2861a3be56
Igor Grešovnik <gresovnik@gmail.com>
2021-07-19 19:55:29 +0200

    Converted broject iglib_base.csproj to SDK project type.

--- igbase/Properties/AssemblyInfo.cs
+++ igbase/Properties/AssemblyInfo.cs
@@ -1,38 +1,38 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿//using System.Reflection;
+//using System.Runtime.CompilerServices;
+//using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("IGLib_Base")]
-[assembly: AssemblyDescription("Investigative Generic Library (by Igor Grešovnik), basic part.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Igor Grešovnik")]
-[assembly: AssemblyProduct("IGLib_Base")]
-[assembly: AssemblyCopyright("Copyright © Igor Grešovnik (2008 - present).")]
-[assembly: AssemblyTrademark("IGLib")]
-[assembly: AssemblyCulture("")]
+//// General Information about an assembly is controlled through the following 
+//// set of attributes. Change these attribute values to modify the information
+//// associated with an assembly.
+//[assembly: AssemblyTitle("IGLib_Base")]
+//[assembly: AssemblyDescription("Investigative Generic Library (by Igor Grešovnik), basic part.")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("Igor Grešovnik")]
+//[assembly: AssemblyProduct("IGLib_Base")]
+//[assembly: AssemblyCopyright("Copyright © Igor Grešovnik (2008 - present).")]
+//[assembly: AssemblyTrademark("IGLib")]
+//[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM vec.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//// Setting ComVisible to false makes the types in this assembly not visible 
+//// to COM vec.  If you need to access a type in this assembly from 
+//// COM, set the ComVisible attribute to true on that type.
+//[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fa4848c3-753a-4538-9322-d6035a74e9d6")]
+//// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("fa4848c3-753a-4538-9322-d6035a74e9d6")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-// See also (espec. T4 templating mechanisim): http://stackoverflow.com/questions/826777/how-to-have-an-auto-incrementing-version-number-visual-studio 
-//  http://stackoverflow.com/questions/356543/can-i-automatically-increment-the-file-build-version-when-using-visual-studio 
-[assembly: AssemblyVersion("1.7.2.*")]
-// [assembly: AssemblyFileVersion("1.6.*")]
+//// Version information for an assembly consists of the following four values:
+////
+////      Major Version
+////      Minor Version 
+////      Build Number
+////      Revision
+////
+//// You can specify all the values or you can default the Build and Revision Numbers 
+//// by using the '*' as shown below:
+//// [assembly: AssemblyVersion("1.0.*")]
+//// See also (espec. T4 templating mechanisim): http://stackoverflow.com/questions/826777/how-to-have-an-auto-incrementing-version-number-visual-studio 
+////  http://stackoverflow.com/questions/356543/can-i-automatically-increment-the-file-build-version-when-using-visual-studio 
+//[assembly: AssemblyVersion("1.7.2.*")]
+//// [assembly: AssemblyFileVersion("1.6.*")]
--- igbase/interfaces/simulations/IResponseEvaluatorVector.cs
+++ igbase/interfaces/simulations/IResponseEvaluatorVector.cs
@@ -1,27 +1,27 @@
-﻿// Copyright (c) Igor Grešovnik, IGLib license; http://www2.arnes.si/~ljc3m2/igor/ioptlib/
+﻿//// Copyright (c) Igor Grešovnik, IGLib license; http://www2.arnes.si/~ljc3m2/igor/ioptlib/
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
 
-using IG.Lib;
+//using IG.Lib;
 
-namespace IG.Num
-{
+//namespace IG.Num
+//{
 
 
-    /// <summary>Interface for simulators and other systems (used e.g. for optimization) that 
-    /// can calculate vector response as a function of vector input parameters.</summary>
-    /// $A Igor Oct08 Aug11;
-    public interface IResponseEvaluatorVectorSimple
-    {
+//    /// <summary>Interface for simulators and other systems (used e.g. for optimization) that 
+//    /// can calculate vector response as a function of vector input parameters.</summary>
+//    /// $A Igor Oct08 Aug11;
+//    public interface IResponseEvaluatorVectorSimple
+//    {
         
-        /// <summary>Calculates simulator's response at the specified input parameters.</summary>
-        /// <param name="inputParameters">Input parameters for which response is calculated.</param>
-        /// <param name="outputValues">Vector object where the calculated response is stored after calculation.</param>
-        void CalculateVectorResponse(IVector inputParameters, ref IVector outputValues);
+//        /// <summary>Calculates simulator's response at the specified input parameters.</summary>
+//        /// <param name="inputParameters">Input parameters for which response is calculated.</param>
+//        /// <param name="outputValues">Vector object where the calculated response is stored after calculation.</param>
+//        void CalculateVectorResponse(IVector inputParameters, ref IVector outputValues);
 
-    }  // interface IResponseEvaluatorVector
+//    }  // interface IResponseEvaluatorVector
 
-}
+//}
