0bbbdd1f0801091b34f8d6b8185e6c013ba54922
igor <gresovnik@gmail.com>
2021-10-21 09:18:43 +0200

    Added a script for updating dependencies to repository root. The script delegates the task to the script contained in the InitIGLibModules project.

---
+++ UpdateDependenciesIGLib.bat
@@ -0,0 +1,18 @@
+
+
+rem This script restores or updates IGLib dependencies from their remote
+rem repositories.
+rem The script delegates the task to the script contained in the 
+rem InitIGLibModules project, which is also used to update the dependencies
+rem via build.
+
+echo.
+echo Restoring / updating internal and external dependencies of IGLib...
+echo Executing:
+echo   call "%~dp0\00_initmoodules\InitIGLibModules\scripts\UpdateIGLibModuleRepos.bat"
+call "%~dp0\00_initmoodules\InitIGLibModules\scripts\UpdateIGLibModuleRepos.bat"
+echo.
+echo  ... IGLib dependencies were updated.
+echo.
+
+
