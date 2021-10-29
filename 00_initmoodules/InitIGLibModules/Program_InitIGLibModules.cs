62f55bde89f1ec955c4ffc44b4fb8be6936674d0
Igor Grešovnik <gresovnik@gmail.com>
2021-10-29 14:14:27 +0200

    Added .net standard targets to InitIGLibModules.csproj, commented out insompatible code (it can be ifdef-ed out in the future)

--- 00_initmoodules/InitIGLibModules/Program_InitIGLibModules.cs
+++ 00_initmoodules/InitIGLibModules/Program_InitIGLibModules.cs
@@ -6,8 +6,8 @@ namespace InitIGLibModules
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Environment.NewLine + "Presence of this executable testifies that: " + Environment.NewLine
-                + "  IGLib modules have been initialized in the build process." + Environment.NewLine);
+            //Console.WriteLine(Environment.NewLine + "Presence of this executable testifies that: " + Environment.NewLine
+            //    + "  IGLib modules have been initialized in the build process." + Environment.NewLine);
         }
     }
 }
