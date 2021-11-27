777436fbf3dc97516ea985cea305322d59119bb3
Igor Grešovnik <gresovnik@gmail.com>
2021-11-28 00:51:01 +0100

    Updated 00_initmoodules/scripts/UpdateIGLibExternal.bat - calls update script from IGLibContainer (workspace/base/UpdateModule_iglibexternal.bat) if it exists.

--- 00_initmoodules/scripts/UpdateIGLibExternal.bat
+++ 00_initmoodules/scripts/UpdateIGLibExternal.bat
@@ -11,6 +11,17 @@ ver > nul
 rem Call bootstrapping script to define basic directories and strings:
 call "%~dp0\..\bootstrappingscripts\BootStrapScripting.bat"
 
+set UpdateScriptContainer=..\..\..\UpdateModule_iglibexternal.bat
+if exist "%UpdateScriptContainer%" (
+  echo.
+  echo IGLib container's update script exists:
+  echo   %UpdateScriptContainer%
+  echo This script will be called to update iglibexternal/
+  echo.
+  call "%UpdateScriptContainer%" %*
+  goto finalize
+)
+
 rem For debugging & trobleshooting - output script locations:
 echo.
 echo Updating IGLib module...
@@ -18,12 +29,14 @@ echo   IGLibScripts: %IGLibScripts%
 echo   UpdateRepo: %UpdateRepo%
 echo.
 
-echo    Updating module IGLibCore ...
+echo    Updating module IGLibExternal ...
 call "%UpdateRepo%" "%~dp0\SettingsIGLibExternal.bat"
 echo.
 
 echo.
 echo   ... updating module completeed.
 echo.
+
+:finalize
 
 endlocal
