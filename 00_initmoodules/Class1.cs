e01e145da8ef4674a2cb0a5fde410576da08c771
Igor Grešovnik <gresovnik@gmail.com>
2021-11-28 23:47:06 +0100

    Rearranged the InitIGLibModules project after some difficulties.

---
+++ 00_initmoodules/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace InitIGLibModules1
+{
+    public class Class1
+    {
+
+    }
+}
--- 00_initmoodules/Program_InitIGLibModules.cs
+++ 00_initmoodules/Program_InitIGLibModules.cs
@@ -4,7 +4,7 @@ namespace InitIGLibModules
 {
     class Program
     {
-        static void Main(string[] args)
+        static void MainInitIGLibModules(string[] args)
         {
             //Console.WriteLine(Environment.NewLine + "Presence of this executable testifies that: " + Environment.NewLine
             //    + "  IGLib modules have been initialized in the build process." + Environment.NewLine);
