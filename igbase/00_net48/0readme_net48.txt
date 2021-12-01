1f92eceb49f64a0d81b7f0f95f005649f2eff1aa
Igor Grešovnik <gresovnik@gmail.com>
2021-12-01 17:42:16 +0100

    Preparing for separating out .NET Framework dependencies...

---
+++ igbase/00_net48/0readme_net48.txt
@@ -0,0 +1,6 @@
+﻿
+This subdirecttory contains source files that depend onn the full .NET framework (.NET Framework 4.8 or earlier).
+
+
+
+
---
+++ igbase/00_net48/Net48.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IG.Lib._00_net48
+{
+    
+    /// <summary>This is just a temporary markerr calss to control exclusion of some stuff.
+    /// Please remove the entire directory later.</summary>
+    internal class Net48
+    {
+    }
+
+}
