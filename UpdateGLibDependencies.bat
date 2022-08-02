0fcce0fbc2e15b7a1a4c4b69e08bd3bd8b0488d4
igor <gresovnik@gmail.com>
2022-08-02 09:30:55 +0200

    Corrected UpdateGLibDependencies.bat

--- UpdateGLibDependencies.bat
+++ UpdateGLibDependencies.bat
@@ -9,8 +9,8 @@ rem via build.
 echo.
 echo Restoring / updating internal and external dependencies of IGLib...
 echo Executing:
-echo   call "%~dp0\00_initmodules\InitIGLibModules\scripts\UpdateIGLibModuleRepos.bat"
-call "%~dp0\00_initmoodules\InitIGLibModules\scripts\UpdateIGLibModuleRepos.bat"
+echo   call "%~dp0\00_initmodules\UpdateIGLibModuleRepos.bat"
+call "%~dp0\00_initmodules\UpdateIGLibModuleRepos.bat"
 echo.
 echo  ... IGLib dependencies were updated.
 echo.
