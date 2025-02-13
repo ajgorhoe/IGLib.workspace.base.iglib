1d8966a31acf24260859b0c03741ce9e7c7bea2d
Igor Grešovnik <gresovnik@gmail.com>
2025-02-13 18:36:43 +0100

    Added InitIGlibModules reference to the IGLib project; commented unnecessary program file in InitIGlibModules

--- 00_initmodules/InitIGLibModules/Program_InitIGLibModules.cs
+++ 00_initmodules/InitIGLibModules/Program_InitIGLibModules.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿//using System;
 
-namespace InitIGLibModules 
-{
-    class Program
-    {
-        static void MainInitIGLibModules(string[] args)
-        {
-            //Console.WriteLine(Environment.NewLine + "Presence of this executable testifies that: " + Environment.NewLine
-            //    + "  IGLib modules have been initialized in the build process." + Environment.NewLine);
-        }
-    }
-}
+//namespace InitIGLibModules 
+//{
+//    class Program
+//    {
+//        static void MainInitIGLibModules(string[] args)
+//        {
+//            //Console.WriteLine(Environment.NewLine + "Presence of this executable testifies that: " + Environment.NewLine
+//            //    + "  IGLib modules have been initialized in the build process." + Environment.NewLine);
+//        }
+//    }
+//}
